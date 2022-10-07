fb83953c784a20f14f63ee2ed67aae54788e3dda
David <davidongaroh@gmail.com>
2022-10-07 14:41:00 +0300

    correct authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
 
     def create
-        user = User.find_by(username: params[:username)
-            if user&.authenticate(password: params[:password)
+        user = User.find_by(username: params[:username])
+            if user&.authenticate(params[:password])
+                session[:user_id] = user.id
                 render json: { message:"User logged in successfully"}, status: :created
             else
                 render json: { message:"Invalid username or password"}, status: :unauthorized
