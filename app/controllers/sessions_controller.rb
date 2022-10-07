68404899854cd5c84cdd1cd6fc601e45c2bedc4e
David <davidongaroh@gmail.com>
2022-10-07 14:33:54 +0300

    add /logout & #destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username)
+            if user&.authenticate(password: params[:password)
+                render json: { message:"User logged in successfully"}, status: :created
+            else
+                render json: { message:"Invalid username or password"}, status: :unauthorized
+            end
+    end
+
+    def destroy
+        session.delete :user_id
+        
+    end
 end
